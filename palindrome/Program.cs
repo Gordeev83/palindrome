d6eaa4c763373424613649ea57e4739392d4e906
Gordeev83 <152768178+Gordeev83@users.noreply.github.com>
2024-03-21 11:33:05 +0300

    Добавьте файлы проекта.

---
+++ palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace palindrome
+{
+    class Program
+    {
+        static bool IsPalindrome(int number)
+        {
+            int reverse = 0;
+            int original = number;
+
+            while (number > 0)
+            {
+                int remainder = number % 10;
+                reverse = reverse * 10 + remainder;
+                number /= 10;
+            }
+
+            return original == reverse;
+        }
+
+        static void Main()
+        {
+            // Примеры вызова метода IsPalindrome
+            Console.WriteLine(IsPalindrome(1221)); // true
+            Console.WriteLine(IsPalindrome(3443));  // true
+            Console.WriteLine(IsPalindrome(7854));  // false
+        }
+    }
+}
